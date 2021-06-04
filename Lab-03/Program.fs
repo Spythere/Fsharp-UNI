21dcbc484bb61384233731455738b29d9f4108bd
yeahaww <antkijj@gmail.com>
2021-06-04 16:22:39 +0200

    add some changes 03 - BST

--- Lab-03/Program.fs
+++ Lab-03/Program.fs
@@ -61,6 +61,7 @@ let result = List.sortBy (fun (x : string) -> -x.Length ) values
 //----------------------
 // Task 3.12
 // 
+
 let revlists xs = List.map (fun x -> List.rev x) xs;;
 
 let example5 () =
@@ -68,7 +69,57 @@ let example5 () =
     let x =revlists [[0;1;1];[3;2];[];[5]]
     printfn "The reverse of %A is %A" list x;;
 
+//----------------------
+// Task 3.16
+// 
 
+
+let rec isSorted list =
+    match list with
+    | [] | [_] -> true
+    | h1::(h2::_ as tail) -> h1 <= h2 && isSorted tail
+
+let task_3_16 () = 
+    let list = [[5;1;5]];
+    printfn "The isSorted of %A is %A" isSorted list;;
+
+//----------------------
+// Task 3.20
+// 
+
+type 'a Tree = Empty | Branch of 'a * 'a Tree * 'a Tree
+
+let rec foldTreeNaive accFun init = function
+    | Empty -> init
+    | Branch (x, left, right) ->
+        let lacc = foldTreeNaive accFun init left
+        let racc = foldTreeNaive accFun init right
+        accFun x lacc racc
+
+let counLeaves tree =
+    foldTreeNaive (fun abc lc rc ->
+        if lc + rc = 0 then 1
+        else 1 + lc + rc) 0 tree
+
+let Tree1 = Branch ('x', Branch ('x', Empty, Empty),Branch ('x', Empty, Branch ('x', Empty, Branch ('x', Empty, Empty))))
+
+
+let result_tree_leaf = counLeaves Tree1
+
+//----------------------
+// Task 3.21
+// 
+type Tree =
+    | Tip
+    | Node of int * Tree * Tree
+
+let rec sumTree tree =
+    match tree with
+    | Tip -> 0
+    | Node(value, left, right) ->
+        value + sumTree(left) + sumTree(right)
+let myTree = Node(0, Node(1, Node(2, Tip, Tip), Node(3, Tip, Tip)), Node(4, Tip, Tip))
+let resultSumTree = sumTree myTree
 
 [<EntryPoint>]
 let main argv =
@@ -96,6 +147,16 @@ let main argv =
     List.iter(fun x -> printfn "%A" x) result
 
     // Print all our results.
-    printfn "Task 3.11 %s" message
+    printfn "Task 3.12 %s" message
     example5 () |> ignore
+
+    printfn "Task 3.16 %s" message
+    task_3_16 () |> ignore
+
+    printfn "Task 3.20 %s" message
+    printfn "%A" result_tree_leaf
+
+    printfn "Task 3.21 %s" message
+    printfn "%A" resultSumTree
+
     0 // return an integer exit code
